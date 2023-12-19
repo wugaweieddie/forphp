c453678e0cb7d78aacd1e2059212cdc5529c9542
Gawei <okm2996088@gmail.com>
2023-12-19 08:23:26 +0800

    no message

--- 20231211-Createdb.php
+++ 20231211-Createdb.php
@@ -1,10 +1,10 @@
 <?php
     header("Access-Control-Allow-Origin: *");
     //接收前端過來的值
-    $p_pname = $_POST["flist"];
-    $p_price = $_POST["ftel"];
-    $p_pnum = $_POST["fcont"];
-    $p_premark = $_POST["fmon"];
+    $p_flist = $_POST["flist"];
+    $p_ftel = $_POST["ftel"];
+    $p_fcont = $_POST["fcont"];
+    $p_fmon = $_POST["fmon"];
 
 
 
@@ -18,7 +18,7 @@
     $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
     //設定錯誤模式
     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    $sql ="INSERT INTO food(flist,ftel,fcont,fmon) VALUES('$p_pname','$p_price','$p_pnum','$p_premark')";
+    $sql ="INSERT INTO food(flist,ftel,fcont,fmon) VALUES('$p_flist','$p_ftel','$p_fcont','$p_fmon')";
     $conn->exec($sql);
     echo"新增成功!";
     }catch(PDOException $e){
